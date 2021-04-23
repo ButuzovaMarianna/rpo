2ce71b7e823f41999184096715e704883d6b965d
ButuzovaMarianna <mbutuz032@gmail.com>
2021-04-23 11:13:43 +0300

    rpo_2

--- chel/app/src/main/java/ru/iu3/chel/MainActivity.java
+++ chel/app/src/main/java/ru/iu3/chel/MainActivity.java
@@ -1,4 +1,5 @@
 package ru.iu3.chel;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,10 +10,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 
 import android.view.View;
 import android.widget.Button;
@@ -35,7 +42,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn = findViewById(R.id.btnClickMe);
-        btn.setOnClickListener((View v) -> { onButtonClick(v);});
+        btn.setOnClickListener((View v) -> {
+            onButtonClick(v);
+        });
+
+        Button btnHttp = findViewById(R.id.btnHttp);
+        btnHttp.setOnClickListener((View v) -> {
+            onButtonHttpClick(v);
+        });
         Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
         // Button btn = findViewById(R.id.btnClickMe);
         // btn.setOnClickListener((View v) -> { onButtonClick(v);});
@@ -68,18 +82,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public native String stringFromJNI();
+
     public static native int initRng();
+
     public static native byte[] randomBytes(int no);
 
     public static native byte[] encrypt(byte[] key, byte[] data);
+
     public static native byte[] decrypt(byte[] key, byte[] data);
 
     public static byte[] StringToHex(String s) {
         byte[] hex;
         try {
             return Hex.decodeHex(s.toCharArray());
-        }
-        catch (DecoderException ex) {
+        } catch (DecoderException ex) {
             hex = null;
         }
         return new byte[0];
@@ -95,5 +111,48 @@ public class MainActivity extends AppCompatActivity {
         // startActivity(it);
 
         startActivityForResult(it, 0);
+    }
+
+    protected void onButtonHttpClick(View v) {
+        TestHttpClient();
+    }
+
+    protected void TestHttpClient() {
+        new Thread(() -> {
+            try {
+                //HttpURLConnection uc = (HttpURLConnection) (new URL("https://www.wikipedia.org").openConnection());
+                HttpURLConnection uc = (HttpURLConnection) (new URL("http://10.0.2.2:8081/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() -> {
+                    Toast.makeText(this, title, Toast.LENGTH_SHORT).show();
+                });
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+
+
+    protected String getPageTitle(String html) {
+        /*int pos = html.indexOf("<title");
+        String p = "not found";
+        if (pos >= 0)
+        {
+            int pos2 = html.indexOf("<", pos + 1);
+            if (pos >= 0)
+                p = html.substring(pos + 7, pos2);
+        }
+        return p;*/
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find()) {
+            p = matcher.group(1);
+        } else {
+            p = "Not Found";
+        }
+        return p;
     }
 }
